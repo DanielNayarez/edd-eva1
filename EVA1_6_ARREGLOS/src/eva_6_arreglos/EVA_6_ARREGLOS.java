fae1acff895512f19fee6d8322b5a6baef0e1954
DanielNayarez <L25550285@chihuahua2.tecnm.mx>
2026-09-10 20:32:17 -0600

    Add files via upload

---
+++ EVA1_6_ARREGLOS/src/eva_6_arreglos/EVA_6_ARREGLOS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_6_arreglos;
+
+/**
+ *
+ * @author nayar
+ */
+public class EVA_6_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] datos = new int[1000000000];// 4 GB
+        System.out.println(datos);
+        for(int i = 0; i < datos.length; i++) {
+            datos[i] = (int)(Math.random()*100);
+        }
+    }
+    
+}
