5f6c9d5563258bbf9e2d51e39f2b386e452bfc7f
DanielNayarez <L25550285@chihuahua2.tecnm.mx>
2026-09-14 16:56:09 -0600

    Add files via upload

---
+++ EVA1_10_4DIMENSIONES/src/eva1_10_4dimensiones/EVA1_10_4DIMENSIONES.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_10_4dimensiones;
+
+/**
+ *
+ * @author nayar
+ */
+public class EVA1_10_4DIMENSIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[][][][] arreglo = new int [2][2][2][2];
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[i].length; j++) {
+                for (int k = 0; k < arreglo[i][j].length; k++) {
+                    for (int l = 0; l < arreglo[i][j][k].length; l++) {
+                        arreglo[i][j][k][l] = (int)(Math.random() * 100);
+                        
+                    }
+                    
+                }
+                
+            }
+        }
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo[i].length; j++) {
+                for (int k = 0; k < arreglo[i][j].length; k++) {
+                    for (int l = 0; l < arreglo[i][j][k].length; l++) {
+                        System.out.print("[" + arreglo [i][j][k][l] + "]");
+                        
+                    }
+                    
+                }
+                
+            }
+        }
+        
+    }
+    
+}
